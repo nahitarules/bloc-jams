82b5da5f69014018607b7448fbddebfc3f53eae4
Nahome Gudeta <nahomegudeta@Nahomes-MacBook-Pro.local>
2017-09-25 13:55:21 -0700

    check9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,4 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
 
           var points = document.getElementsByClassName('point');
 
@@ -14,5 +16,19 @@ var animatePoints = function() {
             revealPoint(i);
           }
 
-
       };
+
+      window.onload = function(){
+        
+        if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+
+      var sellingPoints = document.getElementsByClassName('selling-points')[0];
+      var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+        window.addEventListener('scroll', function(event) {
+          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+       });
+ }
