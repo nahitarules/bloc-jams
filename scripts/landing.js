f88aef68a7adf9b27327eaed9b30e0bac39e84f6
Nahome Gudeta <nahomegudeta@Nahomes-MacBook-Pro.local>
2017-09-26 14:30:02 -0700

    assignment9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,27 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
+          var revealPoint = function(points){
 
-          var points = document.getElementsByClassName('point');
-
-          var revealPoint = function(index){
-
-            points[index].style.opacity = 1;
-            points[index].style.transform = "scaleX(1) translateY(0)";
-            points[index].style.msTransform = "scaleX(1) translateY(0)";
-            points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+            points.style.opacity = 1;
+            points.style.transform = "scaleX(1) translateY(0)";
+            points.style.msTransform = "scaleX(1) translateY(0)";
+            points.style.WebkitTransform = "scaleX(1) translateY(0)";
           }
 
-          for (var i = 0; i < points.length; i++) {
-            revealPoint(i);
+          var animatePoints = function(points){
+            forEach(points,revealPoint);
           }
-
       };
 
       window.onload = function(){
-        
+
         if (window.innerHeight > 950) {
          animatePoints(pointsArray);
      }
 
-      var sellingPoints = document.getElementsByClassName('selling-points')[0];
-      var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
         window.addEventListener('scroll', function(event) {
-          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+          if (pointsArray[0].getBoundingClientRect().top <= 500) {
             animatePoints(pointsArray);
         }
        });
---
+++ scripts/utilities.js
@@ -0,0 +1,12 @@
+var forEach = function(){
+    for(i = 0; i < points.length; i++){
+      points[i];
+    }
+
+}
+
+function forEach(array, callback){
+  for (var i = 0; i < array.length; i++){
+    callback(array[i]);
+  }
+}
