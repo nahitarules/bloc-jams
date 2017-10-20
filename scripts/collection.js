be5ebd959b6574ea2727a441b3a39b983f9bc7e5
Nahome Gudeta <nahomegudeta@Nahomes-MacBook-Pro.local>
2017-10-20 14:26:59 -0700

    check17 complete

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function() {
+     var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -14,14 +15,20 @@ var collectionItemTemplate =
    + '</div>'
    ;
 
-   window.onload = function() {
+
+   return $(template);
+ };
+
+   $(window).load(function() {
     // #1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
+    var $collectionContainer = $('.album-covers');
     // #2
-    collectionContainer.innerHTML = '';
+    $collectionContainer.empty();
 
     // #3
     for (var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbnail = buildCollectionItemTemplate();
+
+        $collectionContainer.append($newThumbnail);
     }
-}
+});
